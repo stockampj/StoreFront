3a033673c2365714c6ad754e3905267a93766d5d
Joel Stockamp <stockampj@gmail.com>
2019-10-27 16:04:27 -0700

    add front end interface and display for products

--- StoreFront/Controllers/HomeController.cs
+++ StoreFront/Controllers/HomeController.cs
@@ -5,14 +5,30 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using StoreFront.Models;
+using StoreFront.Data;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
 
 namespace StoreFront.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _db;
+
+        public HomeController(ApplicationDbContext db)
         {
-            return View();
+            _db = db;
+        }
+        public ActionResult Index()
+        {
+            var treats = _db.Treats
+                .Include(treat => treat.Flavors)
+                .ThenInclude(join => join.Flavor);
+            return View(treats);
         }
 
         public IActionResult Privacy()
